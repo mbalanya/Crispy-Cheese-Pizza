3bcb48b4a3b8742814c9ae927cd601c1f53c12c6
mbalanya <okobacharles@yahoo.co.uk>
2020-05-25 19:20:24 +0300

    add jQuery to link pizza size to list of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,29 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+/*
+$(document).ready(function() {
+  $('option #pizzaSmall').click(function() {
+    alert('you');
+  //  $('form a').prop('href', '#toppingsMedium')
+  });
+});*/
+
+$(document).ready(function() {
+  $('#selectSize').click(function() {
+    var z = $('#selectSize option:selected').text() ;
+     if(z === 'Small (Ksh.500)'){
+       $('form a').removeProp();
+       $('form a').prop('href', '#toppingsSmall');
+     }else if(z === 'Medium (Ksh.800)'){
+       $('form a').removeProp();
+       $('form a').prop('href', '#toppingsMedium');
+     }else if(z === 'Large (Ksh.1200)'){
+       $('form a').removeProp();
+       $('form a').prop('href', '#toppingsLarge');
+     }else{
+       $('form a').prop('href', '');
+       alert('Re-select a valid pizza size')
+     }
+  });
+});
