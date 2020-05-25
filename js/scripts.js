df85317b4ff3538049ff2fb546942e60d5363d13
mbalanya <okobacharles@yahoo.co.uk>
2020-05-25 19:50:04 +0300

    start on business logic

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,11 @@ $(document).ready(function() {
   });
 });*/
 
+
+//links pizza size to relevant toppings
 $(document).ready(function() {
   $('#selectSize').click(function() {
-    var z = $('#selectSize option:selected').text() ;
+    var z = $('#selectSize option:selected').text();
      if(z === 'Small (Ksh.500)'){
        $('form a').removeProp();
        $('form a').prop('href', '#toppingsSmall');
@@ -33,4 +35,28 @@ $(document).ready(function() {
        alert('Re-select a valid pizza size')
      }
   });
+});
+
+function TotalCost(numberOfPizzas, pizzaSize, crustType, numberOfToppings){
+  this.numberOfPizzas = numberOfPizzas;
+  this.pizzaSize = pizzaSize;
+  this.crustType = crustType;
+  this.numberOfToppings = numberOfToppings;
+};
+
+var firstCustomer = new TotalCost(2, 'small', 'crunchy', 2);
+
+TotalCost.prototype.firstCustomerPrint = function(){
+  alert('Hi! You have ordered ' + this.numberOfPizzas + ' pizzas. ' + 'Each pizza is ' + this.pizzaSize + ', with a ' + this.crustType + ' crust and ' + this.numberOfToppings + ' toppings');
+}
+
+firstCustomer.firstCustomerPrint();
+
+$(document).ready(function() {
+  $('form').submit(function(
+    var numberOfPizzasInput = $('#selectPizzaNumbers option:selected').text();
+    var pizzaSizeInput = $('#selectSize option:selected').text();
+    var crustTypeInput = $('#selectCrustType option:selected').text();
+    var number
+  ));
 });
